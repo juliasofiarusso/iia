1715edb8304e4589f3bc8691076b9c33d27d27ce
julia russo <julia russo>
2017-02-13 23:54:48 +0000

    tentativa1

---
+++ #1/Assets/Scripts/CarBehaviour2a.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour2a : CarBehaviour {
+	
+	void Update()
+	{
+		//Read sensor values
+		float leftSensor = LeftLD.GetLinearOutput (); //força do detector de luz esq
+		float rightSensor = RightLD.GetLinearOutput (); //força do detector de luz dir
+
+		//Calculate target motor values
+		m_LeftWheelSpeed = leftSensor * MaxSpeed;
+		m_RightWheelSpeed = rightSensor * MaxSpeed;
+	}
+}

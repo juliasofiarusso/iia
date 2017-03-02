a5f22f698e1145194ab1f5cd01b83ee45922a45b
martavasconcelos <martavasconcelos14@gmail.com>
2017-03-02 21:41:24 +0000

    cenas

---
+++ TrabalhoPratico#1/Assets/Scripts/CarBehaviour_oito.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour_oito : CarBehaviour {
+	public float a;
+	public float b;
+	public float leftSensor;
+	public float rightSensor;
+	void Update()
+	{
+		//Read sensor values
+	leftSensor = LeftLD.GetGaussianOutput (); //força do detector de luz esq
+	rightSensor = RightLD.GetGaussianOutput (); //força do detector de luz dir
+
+		//Calculate target motor values
+//		m_LeftWheelSpeed = leftSensor * MaxSpeed;
+//		m_RightWheelSpeed = rightSensor * MaxSpeed;
+
+	
+		if (leftSensor > 0.25f && leftSensor < 0.75f) {
+			m_LeftWheelSpeed = (1.0f / (b * Mathf.Sqrt (2 * Mathf.PI))) * Mathf.Exp (-1f * Mathf.Pow (leftSensor - a, 2.0f) / (2 * Mathf.Pow (b, 2.0f))) * MaxSpeed;
+		} else {
+			m_LeftWheelSpeed = 0;
+		}
+		if (rightSensor > 0.25f && rightSensor < 0.75f) {
+			m_RightWheelSpeed = (1.0f / (b * Mathf.Sqrt (2 * Mathf.PI))) * Mathf.Exp (-1f * Mathf.Pow (rightSensor - a, 2.0f) / (2 * Mathf.Pow (b, 2.0f))) * MaxSpeed;
+		} else {
+			m_RightWheelSpeed = 0;
+		}
+
+
+	}
+}
